5cda80ca0b3bd63a1fe82d25d4b1d136943aeca6
caro <cirier.c@gmail.com>
2024-04-01 20:52:01 +0200

    marges

--- script.js
+++ script.js
@@ -14,11 +14,12 @@ fetch("https://hubeau.eaufrance.fr/api/v1/niveaux_nappes/chroniques?code_bss=094
       
     createGauge(firstYearLevel,gaugeMax); // call createGauge()
     createGauge(secondYearLevel,gaugeMax);     
+    console.log(mesures)
 });
 
 
 function rate(level, max){  
-  return Math.floor((level/max)*100); // calculate filling rate of water table 
+  return Math.floor((level/max)*100); //  calculate filling rate of water table 
 }
 
 
@@ -26,7 +27,7 @@ function createGauge(level,max){
   const gauges = document.getElementById('gauges');
   const parentDiv = document.createElement("div");
   const childSpan = document.createElement("span"); 
-  const waterLevel = rate(level,max); //holds result of function rate()
+  const waterLevel = rate(level,max); //  holds result of function rate()
   
   parentDiv.setAttribute('class', 'gauge');
 
@@ -36,3 +37,29 @@ function createGauge(level,max){
   parentDiv.appendChild(childSpan);
   gauges.appendChild(parentDiv);
 }
+
+
+async function initMap() {
+  let map;
+  const position = { lat: 43.9738383, lng: 4.6903765 }; // The location of Rochefort
+  const { Map } = await google.maps.importLibrary("maps");  // Request needed libraries
+  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+
+  map = new Map(document.getElementById("map"), { // The map, centered at Rochefort
+    zoom: 10,
+    center: position,
+    mapId: "ROCHEFORT_MAP_ID",
+  });
+
+  const marker = new AdvancedMarkerView({ // The marker, positioned at Rochefort
+    map: map,
+    position: position,
+    title: "Rochefort",
+  });
+}
+
+initMap();
+
+
+
+
