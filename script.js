d4f9b7da9a472b297e2c130ad46125ce8c651f5b
caro <cirier.c@gmail.com>
2024-04-01 16:32:28 +0200

    translation + colors + tag a -> div

--- script.js
+++ script.js
@@ -1,49 +1,38 @@
-// fetch= initie une promesse : appelle l'api 
+// fetch= initialize a promise - API call 
 fetch("https://hubeau.eaufrance.fr/api/v1/niveaux_nappes/chroniques?code_bss=09405X0229/S3&date_debut_mesure=2014-01-20&date_fin_mesure=2024-01-20&size=20000")
 
-// .then = quand reponse de fetch (dc de l'API): retourner l'argument en .json
+// .then = when fetch answers (so do the API): return argument in .json
 .then(response => { 
-    return response.json()
+    return response.json();
 })
 
-
-// aussi quand reponse fetch: executer la boucle for of (= pour chaque ligne de mesures.data, faire ces instructions)
+// .then : execute loop for of 
 .then((mesures)=>{
-  console.log(mesures)
-    const jaugeMax= mesures.data[0].profondeur_nappe +mesures.data[0].niveau_nappe_eau
-    const firstYearLevel = mesures.data[0].niveau_nappe_eau
-    const secondYearLevel = mesures.data[mesures.data.length-1].niveau_nappe_eau
+    const gaugeMax= mesures.data[0].profondeur_nappe +mesures.data[0].niveau_nappe_eau;
+    const firstYearLevel = mesures.data[0].niveau_nappe_eau;
+    const secondYearLevel = mesures.data[mesures.data.length-1].niveau_nappe_eau;
       
-    creerJauge(firstYearLevel,jaugeMax);
-    creerJauge(secondYearLevel,jaugeMax);
-        
+    createGauge(firstYearLevel,gaugeMax); // call createGauge()
+    createGauge(secondYearLevel,gaugeMax);     
 });
 
 
-
-
-
-
-function pourcentage(niveau, jaugeMax){  //recupere proportion de remplissage de l'eau sur la profondeur totale de la nappe
-  
-  return Math.floor((niveau/jaugeMax)*100) // calcule taux de remplissage en eau de la nappe MEETmm
-  
+function rate(level, max){  
+  return Math.floor((level/max)*100); // calculate filling rate of water table 
 }
 
 
-function creerJauge(niveau,jaugeMax){ // parametre = taux de remplissage à appeler de la fonction pourcentage()
+function createGauge(level,max){ 
+  const gauges = document.getElementById('gauges');
+  const parentDiv = document.createElement("div");
+  const childSpan = document.createElement("span"); 
+  const waterLevel = rate(level,max); //holds result of function rate()
   
-  const jauges = document.getElementById('jauges')    // pour se placer dans l'id "jauges" dans HTML
-  const a = document.createElement("a")   // creer balise a
-  const span= document.createElement("span"); // cree balise span
-  const niveauxEau=pourcentage(niveau,jaugeMax) //resultat fonction pourcentage()
-  
-  a.setAttribute('class', 'jauge'); // ajoute attribut class avec valeur jauge ds balise a
-  a.setAttribute('href', '#') // ajoute attribut href avec valeur # ds balise a
+  parentDiv.setAttribute('class', 'gauge');
 
-  span.setAttribute('class', 'jauge-remplissage'); //idem methode a
-  span.setAttribute('style', 'height: '+niveauxEau+'%');
+  childSpan.setAttribute('class', 'gauge-filling');
+  childSpan.setAttribute('style', 'height: ' + waterLevel + '%');
   
-  a.appendChild(span) // ajoute balise span dans balise a
-  jauges.appendChild(a) // ajoute balise a dans balise jauges
+  parentDiv.appendChild(childSpan);
+  gauges.appendChild(parentDiv);
 }
